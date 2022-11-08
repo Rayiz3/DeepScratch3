a5476f494bcebf909e0b32f54b60890a5fa81eb7
Rayiz3 <jusmint3@gmail.com>
2022-11-08 12:06:31 +0900

    add testcase for axis / keepdims

--- steps/step39_Sum.py
+++ steps/step39_Sum.py
@@ -8,6 +8,11 @@ import dezero.functions as F
 
 
 x = Variable(np.array([[1,2,3], [4,5,6]]))
-y = F.sum(x)
-y.backward(retain_grad=True)
+y = F.sum(x, axis=0)
+y.backward()
+print(y)
 print(x.grad)
+
+x = Variable(np.random.randn(2,3,4,5))
+y = x.sum(keepdims=True)
+print(y.shape)
