53eab0147b0bc684edc4ef3294f50fc61d2521b1
Rayiz3 <jusmint3@gmail.com>
2022-11-14 08:51:54 +0900

    new function : exp(), log(), clip(), max(), softmax_cross_entropy(), softmax()

--- dezero/functions.py
+++ dezero/functions.py
@@ -30,7 +30,38 @@ class Tanh(Function):
     def backward(self, gy):
         x, = self.inputs
         return gy * (1 - tanh(x) ** 2)
-    
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        y = self.outputs[0]()  # weakref
+        return gy * y
+
+class Log(Function):
+    def forward(self, x):
+        y = np.log(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        return gy / x
+
+class Clip(Function):
+    def __init__(self, x_min, x_max):
+        self.x_min = x_min
+        self.x_max = x_max
+
+    def forward(self, x):
+        y = np.clip(x, self.x_min, self.x_max)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        mask = (x.data >= self.x_min) * (x.data <= self.x_max)
+        return gy * mask
+
 # =====================
 # Matrix topology
 # =====================
@@ -100,6 +131,26 @@ class Sum(Function):
     def backward(self, gy):
         gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
         return broadcast_to(gy, self.x_shape)
+
+class Max(Function):
+    def __init__(self, axis=None, keepdims=False):
+        self.axis = axis
+        self.keepdims = keepdims
+
+    def forward(self, x):
+        y = x.max(axis=self.axis, keepdims=self.keepdims)
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0]
+        y = self.outputs[0]()  # weakref
+
+        shape = utils.max_backward_shape(x, self.axis)  # e.g.) axis = 1 : [[a], [b], [c]]
+        gy = reshape(gy, shape)
+        y = reshape(y, shape)
+        cond = (x.data == y.data)  # x: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] <=> y: [[3], [6], [9]]
+        gy = broadcast_to(gy, cond.shape)
+        return gy * cond
     
 class BroadcastTo(Function):
     def __init__(self, shape):
@@ -159,6 +210,25 @@ class MeanSquaredError(Function):
         gx = gy * (2. / len(diff)) * diff
         return gx, -gx
 
+class SoftmaxCrossEntropy(Function):
+    def forward(self, x, t):
+        N = x.shape[0]
+        
+        p = clip(softmax(x), 1e-15, 1.0)  # clip() : x range is fixed with (1e-15 ~ 1.0)
+        logp = log(p)
+        tlogp = logp[np.arange(N), t]
+        y = -1/N * tlogp.sum()
+        return y.data
+
+    def backward(self, gy):
+        x, t = self.inputs
+        N, class_N = x.shape
+
+        y = softmax(x)
+        t_onehot = np.eye(class_N, dtype=t.dtype)[t.data]  # convert t to one-hot vector
+        y = 1/N * gy * (y - t_onehot)
+        return y
+
 # =====================
 # Activation function
 # =====================
@@ -173,6 +243,22 @@ class Sigmoid(Function):
         gx = gy * y * (1 - y)
         return gx
 
+class Softmax(Function):
+    def __init__(self, axis=1):
+        self.axis = axis
+
+    def forward(self, x):
+        y = x - x.max(axis=self.axis, keepdims=True)  # prevented : overflow
+        y = np.exp(y)
+        y /= y.sum(axis=self.axis, keepdims=True)  # broadcast to each axis  e.g.) [[a], [b]] => [[0.2, 0.3, 0.5], [0.1, 0.3, 0.6]]
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = y * gy
+        sumdx = gx.sum(axis=self.axis, keepdims=True)
+        gx -= y * sumdx
+        return gx  # y * gy - y * sum(y * gy)
 
 # rapper functions #
 def sin(x):
@@ -183,6 +269,15 @@ def cos(x):
 
 def tanh(x):
     return Tanh()(x)
+
+def exp(x):
+    return Exp()(x)
+
+def log(x):
+    return Log()(x)
+
+def clip(x, x_min, x_max):
+    return Clip(x_min, x_max)(x)
 
 def reshape(x, shape):
     if x.shape == shape:
@@ -201,6 +296,9 @@ def get_item_grad(x, slices, in_shape):
 def sum(x, axis=None, keepdims=False):
     return Sum(axis, keepdims)(x)
 
+def max(x, axis=None, keepdims=False):
+    return Max(axis, keepdims)(x)
+
 def broadcast_to(x, shape):
     if x.shape == shape:
         return as_variable(x)
@@ -217,8 +315,14 @@ def matmul(x, W):
 def mean_squared_error(x0, x1):
     return MeanSquaredError()(x0, x1)
 
+def softmax_cross_entropy(x, t):
+    return SoftmaxCrossEntropy()(x, t)
+
 def linear(x, W, b=None):
     return Linear()(x, W, b)
 
 def sigmoid(x):
     return Sigmoid()(x)
+
+def softmax(x, axis=1):
+    return Softmax(axis)(x)
