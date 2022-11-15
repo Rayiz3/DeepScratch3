40e290a39650d1e9621638c5b2512793ec25339a
Rayiz3 <jusmint3@gmail.com>
2022-11-15 11:31:53 +0900

    minor typos

--- dezero/dataloaders.py
+++ dezero/dataloaders.py
@@ -18,7 +18,7 @@ class DataLoader:
         if self.shuffle:
             self.index = np.random.permutation(len(self.dataset))
         else :
-            self.index = np.arange(len(Self.dataset))
+            self.index = np.arange(len(self.dataset))
     
     def __iter__(self):
         return self
@@ -30,13 +30,15 @@ class DataLoader:
 
         # take batch
         i, batch_size = self.iteration, self.batch_size
-        batch_index = index[i * batch_size:(i + 1) * batch_size]  # 'batch_size' data
+        batch_index = self.index[i * batch_size:(i + 1) * batch_size]  # 'batch_size' data
         batch = [self.dataset[i] for i in batch_index]
         batch_x = np.array([element[0] for element in batch])
         batch_t = np.array([element[1] for element in batch])
         
         # update
         self.iteration += 1
+        
+        return batch_x, batch_t
     
     def next(self):
         return self.__next__()
