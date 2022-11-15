ee94dc9309acc37bdf262b41bb5154a81b8de27e
Rayiz3 <jusmint3@gmail.com>
2022-11-15 09:23:50 +0900

    new dataset : Spiral

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -1,5 +1,30 @@
 import numpy as np
 
+
+class Dataset:
+    def __init__(self, train=True):
+        self.train = train
+        self.data = None
+        self.label = None
+        self.prepare()
+    
+    def __getitem__(self, index):
+        assert np.isscalar(index)
+        if self.label is None:
+            return self.data[index], None
+        else:
+            return self.data[index], self.label[index]
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def prepare(self):
+        pass
+
+class Spiral(Dataset):
+    def prepare(self):
+        self.data, self.label = get_spiral(self.train)
+    
 # =============================================================================
 # Toy datasets
 # =============================================================================
