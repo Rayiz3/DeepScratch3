8cb3b5845b2933632cfc2bbbb3675296bcc7e0bc
Rayiz3 <jusmint3@gmail.com>
2022-11-15 11:17:11 +0900

    new function : accuracy()

--- dezero/functions.py
+++ dezero/functions.py
@@ -326,3 +326,14 @@ def sigmoid(x):
 
 def softmax(x, axis=1):
     return Softmax(axis)(x)
+
+# =====================
+# indifferentiable function
+# =====================
+def accuracy(y, t):
+    y, t = as_variable(y), as_variable(t)
+
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
