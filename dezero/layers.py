aa3861bf45d8b88f0f3112f960728b30ca7c24f1
Rayiz3 <jusmint3@gmail.com>
2022-11-23 10:52:19 +0900

    new layer : Conv2d()

--- dezero/layers.py
+++ dezero/layers.py
@@ -102,3 +102,37 @@ class Linear(Layer):
             self._init_W()  # make W
         
         return F.linear(x, self.W, self.b)
+    
+class Conv2d(Layer):
+    def __init__(self, out_channels, kernel_size, stride=1, pad=0, nobias=False, dtype=np.float32, in_channels=None):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.dtype = dtype
+        
+        # parameter initilize - W
+        self.W = Parameter(None, name='W')
+        if in_channels is not None:  # suspend to forward() if in_size is not determined
+            self._init_W()
+        # parameter initilize - b
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(out_channels, dtype=dtype), name='b')
+    
+    def _init_W(self, xp=np):
+        C, OC = self.in_channels, self.out_channels
+        KH, KW = pair(self.kernel_size)
+        scale = np.sqrt(1 / (C * KH * KW))
+        self.W.data = xp.random.randn(OC, C, KH, KW).astype(self.dtype) * scale
+    
+    def forward(self, x):
+        if self.W.data is None:  # when it is suspended
+            self.in_channels = x.shape[1]
+            xp = cuda.get_array_module(x)
+            self._init_W(xp)
+        
+        return F.functions_conv.conv2d(x, self.W, self.b, self.stride, self.pad)
