2583ad01bc3640fc82ba1a8ec59c5380e3e09a62
Rayiz3 <jusmint3@gmail.com>
2022-11-26 11:17:27 +0900

    new dataloader : SeqDataLoader

--- dezero/dataloaders.py
+++ dezero/dataloaders.py
@@ -45,3 +45,23 @@ class DataLoader:
     
     def next(self):
         return self.__next__()
+
+class SeqDataLoader(DataLoader):
+    def __init__(self, dataset, batch_size, gpu=False):
+        super().__init__(dataset=dataset, batch_size=batch_size, shuffle=False, gpu=gpu)  # serial data => do not shuffle
+    
+    def __next__(self):
+        if self.iteration >= self.max_iter:
+            self.reset()
+            raise StopIteration
+    
+        jump = self.data_size // self.batch_size  # number of batch
+        batch_index = [(i * jump + self.iteration) % self.data_size for i in range(self.batch_size)]  # [j n+j 2n+j ... (B-1)n+j]
+        batch = [self.dataset[i] for i in batch_index]
+        
+        xp = cuda.array if self.gpu else np
+        x = xp.array([data[0] for data in batch])
+        t = xp.array([data[1] for data in batch])
+        
+        self.iteration += 1
+        return x, t
